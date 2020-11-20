87a21049affd471ed3962590e1afbe72dd67d0de
Cristian Goina <goinac@hhmi.org>
2020-11-19 23:37:10 -0500

    memmap raw file in read only mode; remove unnecessary objects from config so that schema validates

--- ecephys_spike_sorting/helpers/create_input_config.py
+++ ecephys_spike_sorting/helpers/create_input_config.py
@@ -1,4 +1,6 @@
-import os, sys, argparse
+import os
+import sys
+import argparse
 from .create_input_json import createInputJson
 from . import SpikeGLX_utils
 
@@ -11,26 +13,26 @@ def main(argv):
     parser.add_argument('output_path', help='Output path path')
     parser.add_argument('--ks_output_path', help='Kilosort output path')
     parser.add_argument('--csb_seed', type=int, default=1, help='Run seed')
-    parser.add_argument('--ks_copy_results', action='store_true', help='Make a copy of the kilosort results for postprocessing')
+    parser.add_argument('--ks_copy_results', action='store_true',
+                        help='Make a copy of the kilosort results for postprocessing')
 
     args = parser.parse_args()
 
-    print('!!!!', args)
     npx_directory = os.path.dirname(args.probe_path)
     name = os.path.basename(args.probe_path)
     baseName = SpikeGLX_utils.ParseTcatName(name)
 
-    info = createInputJson(args.input_json, 
-            npx_directory=npx_directory, 
-            continuous_file = os.path.join(npx_directory, name),
-            spikeGLX_data = 'True',
-            kilosort_output_directory=args.output_path, 
-            ks_make_copy = args.ks_copy_results,
-            extracted_data_directory = npx_directory,
-            noise_template_use_rf = False,
-            CSBseed = args.csb_seed)
+    info = createInputJson(args.input_json,
+                           npx_directory=npx_directory,
+                           continuous_file=os.path.join(npx_directory, name),
+                           spikeGLX_data='True',
+                           kilosort_output_directory=args.output_path,
+                           ks_make_copy=args.ks_copy_results,
+                           extracted_data_directory=npx_directory,
+                           noise_template_use_rf=False,
+                           CSBseed=args.csb_seed)
     print(info)
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
-
