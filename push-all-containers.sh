e418a4d29a017cf0293c802bca07f628a2e4b5bb
Cristian Goina <goinac@hhmi.org>
2021-03-04 12:48:41 -0500

    script to push containers to Janelia's registry

---
+++ push-all-containers.sh
@@ -0,0 +1,5 @@
+docker push registry.int.janelia.org/janeliascicomp/ecephys-modules:1.0.0
+docker push registry.int.janelia.org/janeliascicomp/catgt:1.0.0
+docker push registry.int.janelia.org/janeliascicomp/cwaves:1.0.0
+docker push registry.int.janelia.org/janeliascicomp/tprime:1.0.0
+docker push registry.int.janelia.org/janeliascicomp/kilosort:1.0.0
