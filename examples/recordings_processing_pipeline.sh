0e73af1f74954633bffc278b0878f30c3a738776
Cristian Goina <goinac@hhmi.org>
2021-03-23 09:46:00 -0400

    extended recordings example

--- examples/recordings_processing_pipeline.sh
+++ examples/recordings_processing_pipeline.sh
@@ -1,10 +1,29 @@
 #!/bin/sh
 
+PROFILE=lsf
+# LSF_PROJECT_CODE set it to the output of `lsfgroup yourusername`
+LSF_PROJECT_CODE=
+# SINGULARITY_RUNTIME_OPTS - typically it contains volumes that must be mounted inside the container
+# When the recordings json is present all directories for the probes present in the file must
+# be accessible inside the container so make sure you either bind all the directories
+# or their common (non-roott) directory
+SINGULARITY_RUNTIME_OPTS=""
+
+# TOWER_ACCCESS_TOKEN is available after login to http://nextflow.int.janelia.org under "Your tokens" menu
+export TOWER_ACCESS_TOKEN=072f8fd02196a0b75f378ef237549c6822c221da
+
+if [[ "$PROFILE" == "lsf"]] ; then
+    PROFILE_ARG="-profile lsf"
+else
+    PROFILE_ARG=
+fi
+
 ./main.nf \
-    -profile localdocker \
+    ${PROFILE_ARG} \
+    -with-tower "http://nextflow.int.janelia.org/api" \
+    --runtime_opts "${SINGULARITY_RUNTIME_OPTS}" \
+    --lsf_opts "-P ${LSF_PROJECT_CODE}" \
     --recordings examples/recordings.json \
-    --data_dir $PWD/../testData \
     --config_dir $PWD/local/test/config \
     --results_dir $PWD/local/test/results \
-    --ks_working_dir /tmp/ks_tmp \
-    --has_aux_data true
+    --ks_working_dir /tmp/ks_tmp
