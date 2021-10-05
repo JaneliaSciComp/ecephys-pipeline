b534fab66252e30085ea9c783b5d82d1ba236d19
Cristian Goina <goinac@hhmi.org>
2021-10-05 11:06:56 -0400

    bump up container version; removed images from the container

--- containers/scripts/create-all-containers.sh
+++ containers/scripts/create-all-containers.sh
@@ -1,3 +1,4 @@
+TOOLS_VERSION=1.0.1
 docker build \
     -t matlab-centos8:2020b \
     -t registry.int.janelia.org/janeliascicomp/matlab-centos8:2020b \
@@ -5,26 +6,26 @@ docker build \
     containers/matlab-dockerfile-centos8
 
 docker build \
-    -t registry.int.janelia.org/janeliascicomp/ecephys-modules:1.0.0 \
-    -t ecephys-modules:1.0.0 \
+    -t registry.int.janelia.org/janeliascicomp/ecephys-modules:${TOOLS_VERSION} \
+    -t ecephys-modules:${TOOLS_VERSION} \
     containers/ecephys-modules
 
 docker build \
-    -t registry.int.janelia.org/janeliascicomp/catgt:1.0.0 \
-    -t catgt:1.0.0 \
+    -t registry.int.janelia.org/janeliascicomp/catgt:${TOOLS_VERSION} \
+    -t catgt:${TOOLS_VERSION} \
     containers/catgt
 
 docker build \
-    -t registry.int.janelia.org/janeliascicomp/cwaves:1.0.0 \
-    -t cwaves:1.0.0 \
+    -t registry.int.janelia.org/janeliascicomp/cwaves:${TOOLS_VERSION} \
+    -t cwaves:${TOOLS_VERSION} \
     containers/cwaves
 
 docker build \
-    -t registry.int.janelia.org/janeliascicomp/tprime:1.0.0 \
-    -t tprime:1.0.0 \
+    -t registry.int.janelia.org/janeliascicomp/tprime:${TOOLS_VERSION} \
+    -t tprime:${TOOLS_VERSION} \
     containers/tprime
 
 docker build \
-    -t registry.int.janelia.org/janeliascicomp/kilosort:1.0.0 \
-    -t kilosort:1.0.0 \
+    -t registry.int.janelia.org/janeliascicomp/kilosort:${TOOLS_VERSION} \
+    -t kilosort:${TOOLS_VERSION} \
     containers/kilosort
