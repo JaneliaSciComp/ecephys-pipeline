3455225af72debc56e25280c81de27180dfea9a9
Cristian Goina <goinac@hhmi.org>
2022-06-02 20:24:58 -0400

    also tag for dockerhub

--- containers/scripts/create-all-containers.sh
+++ containers/scripts/create-all-containers.sh
@@ -6,6 +6,7 @@ if [[ "$1" == "--include-matlab" ]] ; then
     echo "Build Matlab docker image"
     docker build \
         -t matlab-oraclelinux8:2020b \
+        -t janeliascicomp/matlab-oraclelinux8:2020b \
         -t registry.int.janelia.org/janeliascicomp/matlab-oraclelinux8:2020b \
         --build-arg LICENSE_SERVER=27000@vm7142.int.janelia.org \
         containers/matlab-oraclelinux8
